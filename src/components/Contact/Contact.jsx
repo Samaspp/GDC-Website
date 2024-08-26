8800210ab7036575ab06254b0442dac8db6687a3
sami <samiap914@gmail.com>
2024-08-26 13:14:03 +0530

    style change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,17 +4,17 @@ import logo from '../../assets/croppedlogo.png';
 
 function Contact() {
   return (
-      <div className='flex flex-col py-4 px-6 w-2/5 mx-auto  h-4/5 bg-black bg-opacity-30 border border-black max-w-screen-md'>
-        <img src={logo} alt='logo' className='rounded-full h-20 object-contain  items-center'/>
-        <h2 className="mb-3 font-navfont text-3xl text-white ">CONTACT US</h2>
-        <p className='mb-4  font-light text-center text-gray-300 sm:text-xl'>Get in touch with us!</p>
+      <div className='flex flex-col py-4 px-6 w-3/4 mx-auto  h-4/5 bg-black bg-opacity-30 border border-black max-w-screen-md md:w-2/5'>
+        <img src={logo} alt='logo' className='rounded-full md:h-20 object-contain  items-center h-0 pb-2'/>
+        <h2 className="sm:mb-3 font-navfont md: text-2xl sm:text-3xl text-white ">CONTACT US</h2>
+        <p className='mb-4  font-light text-center text-gray-300 sm:text-xl text-sm'>Get in touch with us!</p>
         <form action="#">
-          <div className="flex flex-row">
-            <div className='w-1/2 pr-2'>
+          <div className="flex sm:flex-row flex-col">
+            <div className='sm:w-1/2 sm:pr-2'>
               <label  for ="firstName" className="text-white hidden"> First Name:</label>
               <input  type="text" className='shadow-sm text-white text-sm rounded-sm block w-full p-2.5  bg-gray-400 bg-opacity-20'  id="fname" placeholder='First Name'required/>
             </div>
-            <div className='w-1/2 pl-2'>
+            <div className='sm:w-1/2 sm:pl-2 mt-4 sm:mt-0 '>
               <label  for ="lastName" className="text-white hidden">Last Name: </label> 
               <input  type="text" className='shadow-sm bg-gray-400 bg-opacity-20 text-white text-sm rounded-sm block w-full p-2.5 '  id="lname" placeholder='Last Name'required/>
             </div>
